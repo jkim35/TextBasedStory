a2c82b16e9d0e674ade20a074596fae11ec5297e
jkim35 <155586579+jkim35@users.noreply.github.com>
2024-10-07 14:56:05 -0400

    man

---
+++ One.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class One{
+    public static void main(String[]args){
+        System.out.println("\\\\\\\\////\\\\\\\\////\\\\\\\\");
+        System.out.println("\\ Hello Traveller! /");
+        System.out.println("\\ Before playing,  /");
+        System.out.println("\\ Please do not    /");
+        System.out.println("\\ Look inside the  /");
+        System.out.println("\\ code before      /");
+        System.out.println("\\ finishing the    /");
+        System.out.println("\\ game. Thank you! /");
+        System.out.println("\\\\\\\\////\\\\\\\\////\\\\\\\\");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sign here: ");
+        String hi = input.nextLine();
+        System.out.println("");
+        System.out.println("Thanks for signing!");
+    }
+}
